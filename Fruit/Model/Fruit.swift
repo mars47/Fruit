f14348ed2ab196b5752d9e0d01379b0051e5b839
Omar <omar.amoako@yahoo.co.uk>
2018-02-02 15:18:02 +0000

    Completed Tableview
    Made Web request to download JSON data to device
    Created 'Fruit' Model class to store fruit information
    Created function that converts JSON Data into Fruit Model Objects
    Made custom TableViewCell to handle Updating UI
    Implemented UITableView Delegate Methods
    Linked the UI in Main.Storyboard to classes

---
+++ Fruit/Model/Fruit.swift
@@ -0,0 +1,45 @@
+//
+//  Fruit.swift
+//  Fruit
+//
+//  Created by Omar  on 01/02/2018.
+//  Copyright © 2018 Omar. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class Fruit: NSObject {
+    
+    private var _type: String!
+    private var _price: Int!
+    private var _weight: Int!
+    
+    var type: String {
+        return _type
+    }
+    
+    var price: Int {
+        return _price
+    }
+    
+    var weight: Int {
+        return _weight
+    }
+    
+//    init( type: String, price: Int, weight: Int) {
+//        
+//        _type = type
+//        _price = price
+//        _weight = weight
+//        
+//    }
+    
+    init(initialiseFruitWith dict: Dictionary<String, Any>) {
+
+        _type = dict["type"] as! String
+        _price = dict["price"] as! Int
+        _weight = dict["weight"] as! Int
+    }
+
+}
